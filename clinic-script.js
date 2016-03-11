f74b12eeb77e336a89e20ffb32305242f0a4a14f
derzorngottes <amy.conchie@gmail.com>
2016-03-11 13:29:29 -0800

    Fixed max

--- clinic-script.js
+++ clinic-script.js
@@ -1,38 +1,35 @@
-window.clinicAddresses = [];
-
-$(document).ready(function(){
-  var myZip = 98122;
-
-  (function(){
-    $('#submit').on('click', function(){
-      var searchZip = $('#address').val();
-
-      $.ajax({
-        url: 'https://locator.aids.gov/data?zip=' + searchZip + '&service=clinics',
-        type: 'GET',
-        dataType: 'jsonp',
-        success: function(data){
-          findServices(data);
-        }
-      });
-
-      function findServices(data){
-        var myData = data;
-
-        for(var i = 0; i < 5; i ++){
-          var newName = (myData.services[1].providers[i].title);
-          var newStreet = (myData.services[1].providers[i].streetAddress);
-          var newCity = (myData.services[1].providers[i].locality);
-          var newState = (myData.services[1].providers[i].region);
-          var newZip = (myData.services[1].providers[i].postalCode).substring(0,5);
-          var thisAddress = newStreet + ', ' + newCity + ' ' + newState + ', ';
-          clinicAddresses[i] = newZip;
-          $('.clinic-results').append('<p>' + newName + '<br>' + thisAddress + '<span id = "address' + i + '">' + newZip + '</span></p>');
-          console.log(clinicAddresses);
-        }
-      }
-
-    }); //on submit
-  })(); //iife
-
-});
+// $(document).ready(function(){
+//   var myZip = 98122;
+//
+//   (function(){
+//     $('#submit').on('click', function(){
+//       var searchZip = $('#address').val();
+//
+//       $.ajax({
+//         url: 'https://locator.aids.gov/data?zip=' + searchZip + '&service=clinics',
+//         type: 'GET',
+//         dataType: 'jsonp',
+//         success: function(data){
+//           findServices(data);
+//         }
+//       });
+//
+//       function findServices(data){
+//         var myData = data;
+//
+//         for(var i = 0; i < 5; i ++){
+//           var newName = (myData.services[1].providers[i].title);
+//           var newStreet = (myData.services[1].providers[i].streetAddress);
+//           var newCity = (myData.services[1].providers[i].locality);
+//           var newState = (myData.services[1].providers[i].region);
+//           var newZip = (myData.services[1].providers[i].postalCode).substring(0,5);
+//           var thisAddress = newStreet + ', ' + newCity + ' ' + newState + ', ';
+//           clinicAddresses[i] = newZip;
+//           $('.clinic-results').append('<p>' + newName + '<br>' + thisAddress + '<span id = "address' + i + '">' + newZip + '</span></p>');
+//         }
+//       }
+//
+//     }); //on submit
+//   })(); //iife
+//
+// });
--- locator-script.js
+++ locator-script.js
@@ -26,20 +26,53 @@ function geocodeAddress(geocoder, resultsMap) {
 }
 
 function geocodeOthers(geocoder, resultsMap){
-  for(var i = 0; i < 5; i++){
-    // var address = window[clinicAddresses][i];
-    // console.log(address);
-    var address = '98112';
-    geocoder.geocode({'address': address}, function(results, status) {
-      if (status === google.maps.GeocoderStatus.OK) {
-        resultsMap.setCenter(results[0].geometry.location);
-        var marker = new google.maps.Marker({
-          map: resultsMap,
-          position: results[0].geometry.location
-        });
-      } else {
-        console.log('Geocode was not successful for the following reason: ' + status);
+//  var clinicAddresses = getList();
+  var searchZip = $('#address').val();
+  var clinicAddresses = [];
+
+  (function(){
+    $.ajax({
+      url: 'https://locator.aids.gov/data?zip=' + searchZip + '&service=clinics',
+      type: 'GET',
+      dataType: 'jsonp',
+      success: function(data){
+        findServices(data);
       }
     });
+
+    function findServices(data){
+      var myData = data;
+      console.log(myData);
+
+      for(var i = 0; i < 5; i ++){
+        var newName = (myData.services[1].providers[i].title);
+        var newStreet = (myData.services[1].providers[i].streetAddress);
+        var newCity = (myData.services[1].providers[i].locality);
+        var newState = (myData.services[1].providers[i].region);
+        var newZip = (myData.services[1].providers[i].postalCode).substring(0,5);
+        var thisAddress = newStreet + ', ' + newCity + ' ' + newState + ', ';
+        clinicAddresses[i] = newZip;
+        $('.clinic-results').append('<p>' + newName + '<br>' + thisAddress + '<span id = "address' + i + '">' + newZip + '</span></p>');
+        console.log(clinicAddresses);
+      }
+      returnlist(clinicAddresses);
+    }
+  })();
+
+  function returnlist(clinicAddresses){
+    for(var i = 0; i < 5; i++){
+      var address = clinicAddresses[i];
+      geocoder.geocode({'address': address}, function(results, status) {
+        if (status === google.maps.GeocoderStatus.OK) {
+          resultsMap.setCenter(results[0].geometry.location);
+          var marker = new google.maps.Marker({
+            map: resultsMap,
+            position: results[0].geometry.location
+          });
+        } else {
+          console.log('Geocode was not successful for the following reason: ' + status);
+        }
+      });
+    }
   }
-};
+}
