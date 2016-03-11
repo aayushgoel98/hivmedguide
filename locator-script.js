13526b737463f3862d9b2470776abbc39be8945b
derzorngottes <amy.conchie@gmail.com>
2016-03-11 09:28:37 -0800

    Got geocoding working on map

--- locator-script.js
+++ locator-script.js
@@ -1,4 +1,5 @@
 $(document).ready(function(){
+
   var myZip = 98122;
 
   console.log('script loaded');
@@ -15,29 +16,37 @@ $(document).ready(function(){
   function findServices(data){
     var myData = data;
     console.log(myData);
-
-    myData.services[1].providers
-
+//    myData.services[1].providers
   }
 
-  var lat = '';
-  var lng = '';
-  var address = 98122;
-  geocoder.geocode( { 'address': address}, function(results, status) {
-    if (status == google.maps.GeocoderStatus.OK) {
-      lat = results[0].geometry.location.lat();
-      lng = results[0].geometry.location.lng();
-    });
-    } else {
-      alert("Geocode was not successful for the following reason: " + status);
-    }
-  });
-  log('Latitude: ' + lat + ' Logitude: ' + lng);
+  // function initialize() {
+  //   geocoder = new google.maps.Geocoder();
+  //   var lat = '';
+  //   var lng = '';
+  //   var address = 98122;
+  //   var geocoder;
+  //   geocoder.geocode( { 'address': address}, function(results, status) {
+  //     if (status == google.maps.GeocoderStatus.OK) {
+  //       lat = results[0].geometry.location.lat();
+  //       lng = results[0].geometry.location.lng();
+  //     } else {
+  //       alert("Geocode was not successful for the following reason: " + status);
+  //     }
+  //   });
+  //
+  //   console.log('Latitude: ' + lat + ' Logitude: ' + lng);
+  //
+  //
+  //
+  //   var marker = new google.maps.Marker({
+  //     position: myLatLng,
+  //     map: map,
+  //     title: 'Hello World!'
+  //   });
+  // }
+  //
+  // initialize();
 
-  var marker = new google.maps.Marker({
-    position: myLatLng,
-    map: map,
-    title: 'Hello World!'
-  });
+
 
 });
