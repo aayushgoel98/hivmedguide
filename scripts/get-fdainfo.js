0e26f65a173c76b50cd7db22de4d800a4e4f8716
derzorngottes <amy.conchie@gmail.com>
2016-05-19 03:40:49 -0700

    Add interaction data

--- scripts/get-fdainfo.js
+++ scripts/get-fdainfo.js
@@ -1,5 +1,46 @@
 $(document).ready(function(){
 
+  var medsByCUI = {
+    'Truvada':'495430',
+    'Ziagen':'495430',
+    'Videx':'495430',
+    'Emtriva':'495430',
+    'Epivir':'495430',
+    'Zerit':'495430',
+    'Viread':'495430',
+    'Viramune':'495430',
+    'Viracept':'495430',
+    'Retrovir':'495430',
+    'Rescriptor':'495430',
+    'Sustiva':'495430',
+    'Intelence':'495430',
+    'Edurant':'495430',
+    'Reyataz':'495430',
+    'Prezista':'495430',
+    'Prezcobix':'495430',
+    'Lexiva':'495430',
+    'Crixivan':'495430',
+    'Norvir':'495430',
+    'Invirase':'495430',
+    'Aptivus':'495430',
+    'Fuzeon':'495430',
+    'Selzentry':'495430',
+    'Tivicay':'495430',
+    'Vitekta':'495430',
+    'Isentress':'495430',
+    'Tybost':'495430',
+    'Epzicom':'495430',
+    'Triumeq':'495430',
+    'Trizivir':'495430',
+    'Evotaz':'495430',
+    'Atripla':'495430',
+    'Genvoya':'495430',
+    'Stribild':'495430',
+    'Complera':'495430',
+    'Combivir':'495430',
+    'Kaletra':'495430'
+  };
+
   var transferMed = readCookie('myMed').toString();
   var thisMed = '';
 
@@ -21,8 +62,35 @@ $(document).ready(function(){
     return null;
   }
 
+  // interactions iife
+  (function() {
+    console.log(medsByCUI[thisMed]);
+    $.ajax({
+      url: 'https://rxnav.nlm.nih.gov/REST/interaction/interaction.json?rxcui=' + medsByCUI[thisMed],
+      type: 'GET',
+      crossOrigin: true,
+      dataType: 'json',
+      success: function(data) {
+        interactionsData(data);
+      }
+    });
+
+    function interactionsData(data) {
+      var drugMixData = data;
+      console.log(drugMixData);
+    }
+
+    // function adverseData(data){
+    //   var thisData = $.map(data, function(el){return el});
+    //
+    //   for(var i = 1; i <= 25; i++){
+    //     $('.med-adverse-events').append('<div class="adverse-div"><p>' + thisData[i].term.toLowerCase() + ':<br>' + thisData[i].count + '</p></div>');
+    //   }
+    // }
+  })();
+
   // adverse events iife
-  (function(){
+  (function() {
     $.ajax({
       url: 'https://api.fda.gov/drug/event.json?search=patient.drug.openfda.brand_name:"' + thisMed + '"&count=patient.reaction.reactionmeddrapt.exact',
       type: 'GET',
